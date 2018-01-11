9a5f8fccce0edeaaffe475c339339af636d0e145
Peeyusha Rathi <peeyusha07rathi@gmail.com>
2018-01-11 16:40:42 +0530

    Checkpoint#1

--- Tf_idf/test_tfidf.py
+++ Tf_idf/test_tfidf.py
@@ -1,63 +1,72 @@
-import nltk
-import string
-import os
-import PyPDF2 
-import textract
-import numpy as np
-import pandas as ps
-
-from pandas import *
-
+from PyPDF2 import PdfFileReader
 from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+import numpy as np
+import sys
+import os
 
-#path = '\PIYUSHA\Desktop\FYP\Dataset\Rape\Rape-Bombay\POCSO'
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
 
-token_dict = {}
-stemmer = PorterStemmer()
+def pdf2text(pdf):
+    '''Iterate over pages and extract text'''
+    text = ''
+    for i in range(pdf.getNumPages()):
+        page = pdf.getPage(i)
+        text = text + page.extractText()
+    return text
 
-def stem_tokens(tokens, stemmer):
-    stemmed = []
-    for item in tokens:
-        stemmed.append(stemmer.stem(item))
-    return stemmed
+def stem_tokenize(document):
+    '''return stemmed words longer than 2 chars and all alpha'''
+    tokens = [stem(w) for w in document.split() if len(w) > 2 and w.isalpha()]
+    return tokens
 
-def tokenize(text):
-    print("here")
-    tokens = nltk.word_tokenize(text)
-    stems = stem_tokens(tokens, stemmer)
-    return stems
-      
-for subdir, dirs, files in os.walk(".", topdown=False):
-    for file in files:
-        if(file != 'test_tfidf.py'):
-            file_path = subdir + os.path.sep + file
-            pdfFileObj = open(file_path,'rb')
-            pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-            num_pages = pdfReader.numPages
-            count = 0
-            text = ""
-            while count < num_pages:
-                pageObj = pdfReader.getPage(count)
-                count +=1
-                text += pageObj.extractText()
-            if text != "":
-               text = text
-            else:
-               text = textract.process(fileurl, method='tesseract', language='eng')
 
-            tokens = word_tokenize(text)
-            punctuations = ['(', ')', ',' , '.', '!', '#', '&', '*', '?', '/','-','_',';']
-            stop_words = stopwords.words('english')
-            keywords = [word for word in tokens if not word in stop_words and not word in punctuations]
-            token_dict[file] = keywords
-        
-tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
-tfs = tfidf.fit_transform(token_dict)
-print (tfs.toarray())
-print ('\n')
-print ('\n')
-print ('\n')
+def tokenize(document):
+    ps = PorterStemmer()
+    '''return words longer than 2 chars and all alpha'''
+    tokens = [ps.stem(w) for w in document.split() if len(w) > 2 and w.isalpha()]
+    return tokens
 
+def build_corpus_from_dir(dir_path):
+    corpus = []
+    for root, dirs, filenames in os.walk(dir_path,topdown=False):
+        for name in filenames:
+            if(name.endswith('PDF')):
+                f  = os.path.join(root, name)
+                pdf = PdfFileReader(f, "rb")
+                document = pdf2text(pdf)
+                corpus.append(document)
+    return corpus
+
+if __name__ == '__main__':
+    corpus = build_corpus_from_dir('.')
+    #corpus=["Hi how are you, what you doingg?", "Hey what's up bro? you are cool","Hi what are you up to? Such a cool day"]
+    
+    vectorizer = TfidfVectorizer(tokenizer = tokenize, stop_words = 'english', max_features = 1000)
+    train_cv = vectorizer.fit_transform(corpus)
+    a=train_cv.toarray()
+    print(train_cv)
+    b = vectorizer.inverse_transform(a)
+    features = vectorizer.get_feature_names()
+    print('\nThis is BBBBBBBBB\n')
+    #print(b)
+    print('\n\n\n\n\n\n')
+    print(features)
+
+    print(len(features))
+    #print(corpus.iloc[0])
+    #indices = np.argsort(vectorizer.idf_)[::-1]
+    
+    '''for doc in corpus:
+        print(' '.join(doc.split())[500])
+        tdm = vectorizer.transform([doc])
+        print('TDM')
+        print('\n')
+        print(tdm)
+        dense = tdm.todense()
+        episode = dense[0].tolist()[0]
+        phrase_scores = [pair for pair in zip(range(0, len(episode)), episode) if pair[1] > 0]
+        sorted_phrase_scores = sorted(phrase_scores, key=lambda t: t[1] * -1)
+        for phrase, score in [(features[word_id], score) for (word_id, score) in sorted_phrase_scores][:20]:
+           print('{0: <20} {1}'.format(phrase, score))'''
